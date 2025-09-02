72ea902ef404782d2f0e20c021bddc6b4a9fcdbe
Pallavi <2438041@cognizant.com>
2025-09-02 17:41:14 +0530

    Added child module to the project

---
+++ maven-assignment/src/main/java/App.java
@@ -0,0 +1,15 @@
+/**
+ * Entry point for the Maven Assignment application.
+ */
+public class App {
+
+    /**
+     * Main method that starts the application.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        // Your logic here
+    	
+    	System.out.println("This is maven Project assignment");
+    }
+}
